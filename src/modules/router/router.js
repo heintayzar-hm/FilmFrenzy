82e1c62cfa3f5bc4a6dce8f9c9e8c08a9d0582c8
Hein Tay Zar <heintayzarhm@gmail.com>
2022-11-22 12:58:37 +0630

    Fix routers

--- src/modules/router/router.js
+++ src/modules/router/router.js
@@ -1,52 +1,99 @@
 import NoPage from '../404.js';
 import routerInstance from './route.js';
 
+/*
+set to el.innerHtml
+*/
+const newPage = (Page, el, obj = {}) => {
+  const page = new Page();
+  el.innerHTML = page.html(obj);
+};
+
+// for 404 pages
+const noPage = (el) => {
+  const page = new NoPage();
+  el.innerHTML = page.html();
+};
+
+// for main navigation
+const navigator = (path) => {
+  // get main
+  const main = document.getElementById('main');
+  let route = path;
+  let hash;
+  if (route.includes('#')) {
+    const arr = route.split('#');
+    [route, hash] = arr;
+  }
+  const routeInfo = routerInstance.routes.filter((rou) => (rou.path === route || `${rou.path}/` === route))[0];
+  if (!routeInfo || path === '404') {
+    noPage(main);
+    // eslint-disable-next-line no-use-before-define
+    router();
+  } else if (routeInfo.params && hash) {
+    newPage(routeInfo.page, main, { hash: Number(window.location.hash) });
+    // eslint-disable-next-line no-use-before-define
+    router();
+  } else if (!routeInfo.params) {
+    newPage(routeInfo.page, main, {});
+    // eslint-disable-next-line no-use-before-define
+    router();
+  } else {
+    noPage(main);
+    // eslint-disable-next-line no-use-before-define
+    router();
+  }
+};
+
+/**
+ * navigate according to button
+ */
+const navigate = (event) => {
+  event.preventDefault();
+  let route = event.target.attributes[0].value;
+  let hash;
+  if (route.includes('#')) {
+    const arr = route.split('#');
+    [route, hash] = arr;
+  }
+  const routeInfo = routerInstance.routes.filter((rou) => (rou.path === route || `${rou.path}/` === route))[0];
+  if (!routeInfo) {
+    window.history.pushState({ path: '404' }, '404', 'error');
+    navigator('/404');
+  } else if (routeInfo.params && hash) {
+    window.history.pushState({ path: routeInfo.path }, document.title, `${routeInfo.path}#${hash}`);
+    navigator(`${routeInfo.path}#${hash}`);
+  } else if (!routeInfo.params) {
+    window.history.pushState({ path: routeInfo.path }, document.title, routeInfo.path);
+    navigator(routeInfo.path);
+  } else {
+    window.history.pushState({ path: '404' }, '404', 'error');
+    navigator('/404');
+  }
+};
+
 const router = () => {
   // get main
   const main = document.getElementById('main');
   // path
   const currentPath = window.location.pathname;
   if (currentPath === '/') {
-    const Page = routerInstance.routes[0].page;
-    const page = new Page();
-    main.innerHTML = page.html();
+    newPage(routerInstance.routes[0].page, main);
   } else {
     // check from routes
     const route = routerInstance.routes.filter((rou) => (rou.path === currentPath || `${rou.path}/` === currentPath))[0];
     if (route) {
       if (route.params && window.location.hash) {
-        const Page = route.page;
-        const page = new Page();
-        main.innerHTML = page.html({ hash: Number(window.location.hash.slice(1)) });
+        newPage(route.page, main, { hash: Number(window.location.hash.slice(1)) });
       } else if (!route.params) {
-        const Page = route.page;
-        const page = new Page();
-        main.innerHTML = page.html();
+        newPage(route.page, main, {});
       } else {
-        const page = new NoPage();
-        main.innerHTML = page.html();
+        noPage(main);
       }
     } else {
-      const page = new NoPage();
-      main.innerHTML = page.html();
+      noPage(main);
     }
   }
-
-  const navigate = (event) => {
-    event.preventDefault();
-    const route = event.target.attributes[0].value;
-    const routeInfo = routerInstance.routes.filter((rou) => (rou.path === route || `${rou.path}/` === route))[0];
-    if (!routeInfo) {
-      window.history.pushState({}, '', 'error');
-      const page = new NoPage();
-      main.innerHTML = page.html();
-    } else {
-      window.history.pushState({}, '', routeInfo.path);
-      const Page = routeInfo.page;
-      const page = new Page();
-      main.innerHTML = page.html();
-    }
-  };
 
   // for the links
   const definedRoutes = Array.from(document.querySelectorAll('[spaLink]'));
@@ -55,4 +102,18 @@ const router = () => {
   });
 };
 
+/**
+ * For popstate
+ */
+
+window.addEventListener('popstate', (e) => {
+  if (e.state && e.state.path) {
+    navigator(e.state.path);
+  }
+});
+
+// for intial state
+const getPath = () => window.location.pathname + window.location.search + window.location.hash;
+const path = getPath();
+window.history.replaceState({ path }, document.title, document.location.href);
 export default router;
--- src/modules/show.js
+++ src/modules/show.js
@@ -1,10 +1,29 @@
 export default class Show {
     html = () => {
-      const hello = 'hlo';
+     // const {title} = data;
       return (`
-            <section id="show">
-                ${hello}
-            </section>`
+      <section class="main-section">
+      <div id="show">
+          <div class="popup-header">
+              <h2>Title</h2>
+              <i class="fa-solid fa-xmark"></i>
+          </div>
+          <div class="movie-img">
+              <img src="test.jpg" alt="movie">
+          </div>
+          <div class="movie-details">
+              <p>Description</p>
+              <div class="small-details">
+                  <span>Rating:</span>
+                  <span>Status:</span>
+                  <span>Released Date:</span>
+                  <span>Language:</span>
+              </div>
+
+          </div>
+      </div>
+     
+  </section>`
       );
     }
 }
